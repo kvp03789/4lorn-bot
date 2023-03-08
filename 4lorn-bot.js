f82c925d25f9f58672817d557795c321585096b7
clay <clay.nichols@gmail.com>
2023-03-08 01:45:13 -0600

    fix error

--- 4lorn-bot.js
+++ 4lorn-bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js")
-const config = require("./config.json")
+// const config = require("./config.json")
 // import {getFirestore } from 'firebase/firestore'
 require('dotenv').config()
 
