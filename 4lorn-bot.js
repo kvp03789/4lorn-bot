55ec1237f5aa194995a010d46c80919cecae354b
clay <clay.nichols@gmail.com>
2023-03-08 01:32:57 -0600

    change to using json for bot

--- 4lorn-bot.js
+++ 4lorn-bot.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js")
+const config = require("./config.json")
 // import {getFirestore } from 'firebase/firestore'
 require('dotenv').config()
 
@@ -108,7 +109,7 @@ client.on("message", msg => {
   
 })
 
-client.login(process.env.BOT_TOKEN)
+client.login(config.token)
 
 
 
